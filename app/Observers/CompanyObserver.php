61087fd199f7820989bfaf4d2723308a47c7b96d
Jefferson Frade <jefferrson.frade@gmail.com>
2025-01-14 11:18:41 -0300

    Adicionado observador de delete a empresa

--- app/Observers/CompanyObserver.php
+++ app/Observers/CompanyObserver.php
@@ -2,7 +2,13 @@
 
 namespace App\Observers;
 
+use App\Models\Company;
+use App\Repositories\UserRepository;
+
 class CompanyObserver
 {
-    //
+    public function deleting(Company $company)
+    {
+        app(UserRepository::class)->customDelete('id_company', $company->id);
+    }
 }
